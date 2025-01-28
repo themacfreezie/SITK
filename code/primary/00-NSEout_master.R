7560f65b94b56276e80749c0debca693582be5db
themacfreezie <62110677+themacfreezie@users.noreply.github.com>
2025-01-28 15:43:58 -0800

    updating pathways

--- code/primary/00-NSEout_master.R
+++ code/primary/00-NSEout_master.R
@@ -1,12 +1,11 @@
 library(here)
 
-here::i_am("code/primary/NSEout_master.R")
+here::i_am("code/primary/00-NSEout_master.R")
 
 # nse outside modeling
 source(here("code", "primary", "01-adfgIR_scale.R"))
 source(here("code", "primary", "02-NSEout_WIDEstandardize.R"))
 source(here("code", "primary", "03-NSEout_observer.R"))
 source(here("code", "primary", "04-pdo_clean.R"))
-source(here("code", "NSEout_MARSSeo.R"))
-source(here("code", "NSEout_MARSSall.R"))
-source(here("code", "NSEout_output.R"))
+source(here("code", "primary", "05-NSEout_modeleval.R"))
+source(here("code", "primary", "06-NSEout_output.R"))
