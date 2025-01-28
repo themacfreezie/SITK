f6ba73c6ada3c685820d9d9628a738c4e04d77df
themacfreezie <62110677+themacfreezie@users.noreply.github.com>
2025-01-28 13:59:42 -0800

    restructuring code folder, updating pathways

--- code/primary/01-adfgIR_scale.R
+++ code/primary/01-adfgIR_scale.R
@@ -1,21 +1,12 @@
 ## SET WORKING DIR & PACKAGES
 
 # import packages
-# library(car)
-# library(cowplot)
-# library(dplyr)
-# library(gghighlight)
-# library(ggplot2)
-# library(gplots)
 library(here)
-# library(lmtest)
-# library(plm)
 library(readxl)
 library(tidyverse)
-# library(tseries)
 
 # create working dir and output folder
-here::i_am("code/adfgIR_scale.R")
+here::i_am("code/primary/01-adfgIR_scale.R")
 options(max.print=10000)
 
 
@@ -23,7 +14,7 @@ options(max.print=10000)
 
 # 1) AWC stream length data
 # pull in data
-streams.df <- read_excel(here("data", "awc_stream.xlsx"), col_names = TRUE)
+streams.df <- read_excel(here("data", "raw", "awc_stream.xlsx"), col_names = TRUE)
 
 # drop extraneous variables from streams.df
 streams.df <- streams.df[-c(1, 4:7)]
@@ -56,13 +47,13 @@ streams.df <- streams.df %>%
 names(streams.df)[names(streams.df) == "LENGTH"] <- "LENGTHm"
 streams.df$LENGTHkm <- (streams.df$LENGTHm/1000)
 
-save(streams.df, file=here("data", "stream_length.Rda"))
+save(streams.df, file=here("data", "clean", "stream_length.Rda"))
 
 
 # 2) escapement data (IR & ADFG)
 # pull in data
-pinks.df <- read_excel(here("data", "adfg_pink.xlsx"), col_names = TRUE)
-indianr.df <- read_excel(here("data", "IndianRiver.xlsx"), sheet = "Stopha 2015 - Table 6", col_names = TRUE)
+pinks.df <- read_excel(here("data", "raw", "adfg_pink.xlsx"), col_names = TRUE)
+indianr.df <- read_excel(here("data", "raw", "IndianRiver.xlsx"), sheet = "Stopha 2015 - Table 6", col_names = TRUE)
 
 # rename vars in indianr.df to match pinks.df
 names(indianr.df)[names(indianr.df) == "Indian River Peak Escapement"] <- "PEAK_COUNT"
@@ -101,5 +92,5 @@ pinksE_sc.df <- pinks.df %>% filter(YEAR %% 2 == 0)
 pinksO_sc.df <- pinks.df %>% filter(YEAR %% 2 != 0)
 
 # save scaled dataframes
-save(pinksE_sc.df, file=here("data", "pinksE_sc.Rda"))
-save(pinksO_sc.df, file=here("data", "pinksO_sc.Rda"))
+save(pinksE_sc.df, file=here("data", "clean", "pinksE_sc.Rda"))
+save(pinksO_sc.df, file=here("data", "clean", "pinksO_sc.Rda"))
