8281089ad1c7cf9f72af4b9f1cf048896b731393
themacfreezie <62110677+themacfreezie@users.noreply.github.com>
2025-02-25 10:49:01 -0800

    update

--- code/development/01-DD_dataclean.R
+++ code/development/01-DD_dataclean.R
@@ -6,7 +6,7 @@ library(readxl)
 library(tidyverse)
 
 # create working dir and output folder
-here::i_am("code/primary/01-adfgIR_scale.R")
+here::i_am("code/development/01-DD_dataclean.R")
 options(max.print=10000)
 
 
