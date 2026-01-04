065a62333aee8ac1e0a558a2a9345fff9b9ac0b9
themacfreezie <62110677+themacfreezie@users.noreply.github.com>
2026-01-04 15:58:12 -0800

    goober peas

--- code/development/Betas/data_clean.R
+++ code/development/Betas/data_clean.R
@@ -10,3 +10,9 @@ options(max.print=2000)
 # 1) SJH releases
 # pull in data
 releases.df <- read_csv(here("data", "raw", "sjh_releases.csv"), col_names = TRUE)
+
+# save SJH releases as .rda
+save(releases.df, file=here("data", "clean", "SJHreleases.Rda"))
+
+# 2) Indian river data
+indianr.df <- read_excel(here("data", "raw", "IndianRiver.xlsx"), sheet = "Stopha 2015 - Table 6", col_names = TRUE)
