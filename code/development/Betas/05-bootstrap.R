b76fd14a691765699eefe8bb480f536bc19aa30e
themacfreezie <62110677+themacfreezie@users.noreply.github.com>
2026-01-15 11:22:07 -0800

    uh duh doi

--- code/development/Betas/05-bootstrap.R
+++ code/development/Betas/05-bootstrap.R
@@ -11,17 +11,27 @@ ssObeta_DFGr <- readRDS(file=here("data", "clean", "ssObeta_DFGr.rds"))
 ssObeta_DFGe <- readRDS(file=here("data", "clean", "ssObeta_DFGe.rds"))
 
 # bootstrap estimates
-bootE_AMPr <- MARSSboot(ssEbeta_AMPr, nboot=100, output="parameters", sim = "parametric")
-bootE_DFGr <- MARSSboot(ssEbeta_DFGr, nboot=100, output="parameters", sim = "parametric")
-bootE_DFGe <- MARSSboot(ssEbeta_DFGe, nboot=100, output="parameters", sim = "parametric")
-bootO_AMPr <- MARSSboot(ssObeta_AMPr, nboot=100, output="parameters", sim = "parametric")
-bootO_DFGr <- MARSSboot(ssObeta_DFGr, nboot=100, output="parameters", sim = "parametric")
-bootO_DFGe <- MARSSboot(ssObeta_DFGe, nboot=100, output="parameters", sim = "parametric")
-
-# save bootstrap estimates
-saveRDS(bootE_AMPr, file=here::here("data", "clean", "bootE_AMPr.rds"))
-saveRDS(bootE_DFGr, file=here::here("data", "clean", "bootE_DFGr.rds"))
-saveRDS(bootE_DFGe, file=here::here("data", "clean", "bootE_DFGe.rds"))
-saveRDS(bootO_AMPr, file=here::here("data", "clean", "bootO_AMPr.rds"))
-saveRDS(bootO_DFGr, file=here::here("data", "clean", "bootO_DFGr.rds"))
-saveRDS(bootO_DFGe, file=here::here("data", "clean", "bootO_DFGe.rds"))
+if(!file.exists(here("data", "clean", "bootE_AMPr.rds"))){
+  bootE_AMPr <- MARSSboot(ssEbeta_AMPr, nboot=100, output="parameters", sim = "parametric")
+  saveRDS(bootE_AMPr, file=here("data", "clean", "bootE_AMPr.rds"))
+}
+if(!file.exists(here("data", "clean", "bootE_DFGr.rds"))){
+  bootE_DFGr <- MARSSboot(ssEbeta_DFGr, nboot=100, output="parameters", sim = "parametric")
+  saveRDS(bootE_DFGr, file=here("data", "clean", "bootE_DFGr.rds"))
+}
+if(!file.exists(here("data", "clean", "bootE_DFGe.rds"))){
+  bootE_DFGe <- MARSSboot(ssEbeta_DFGe, nboot=100, output="parameters", sim = "parametric")
+  saveRDS(bootE_DFGe, file=here("data", "clean", "bootE_DFGe.rds"))
+}
+if(!file.exists(here("data", "clean", "bootO_AMPr.rds"))){
+  bootO_AMPr <- MARSSboot(ssObeta_AMPr, nboot=100, output="parameters", sim = "parametric")
+  saveRDS(bootO_AMPr, file=here("data", "clean", "bootO_AMPr.rds"))
+}
+if(!file.exists(here("data", "clean", "bootO_DFGr.rds"))){
+  bootO_DFGr <- MARSSboot(ssObeta_DFGr, nboot=100, output="parameters", sim = "parametric")
+  saveRDS(bootO_DFGr, file=here("data", "clean", "bootO_DFGr.rds"))
+}
+if(!file.exists(here("data", "clean", "bootO_DFGe.rds"))){
+  bootO_DFGe <- MARSSboot(ssObeta_DFGe, nboot=100, output="parameters", sim = "parametric")
+  saveRDS(bootO_DFGe, file=here("data", "clean", "bootO_DFGe.rds"))
+}
--- code/development/Betas/06-boot_output.R
+++ code/development/Betas/06-boot_output.R
@@ -1,7 +1,7 @@
 ## SET WORKING DIR & PACKAGES
-library(gridExtra)
 library(here)
 library(reshape2)
+library(str2str)
 library(tidyverse)
 
 # load model objects
@@ -59,6 +59,31 @@ bplot_tight
 ggsave(here("output", "figures", "E_AMPr_bplot.png"), plot=bplot, device="png", dpi=300)
 ggsave(here("output", "figures", "E_AMPr_bplottight.png"), plot=bplot_tight, device="png", dpi=300)
 
+# dropping outliers and recalculating points
+df$HHz <- scale(df$`Hatchery(t)<<Hatchery(t-1)`)
+df$RHz <- scale(df$`River(t)<<Hatchery(t-1)`)
+df$HRz <- scale(df$`Hatchery(t)<<River(t-1)`)
+df$RRz <- scale(df$`River(t)<<River(t-1)`)
+
+dfHH <- df[, -c(2:4, 6:8)]
+dfRH <- df[, -c(1,3,4,5,7,8)]
+dfHR <- df[, -c(1,2,4,5,6,8)]
+dfRR <- df[, -c(1:3, 5:7)]
+
+dfHH <- dfHH[abs(dfHH$HHz) < 3, ]
+dfRH <- dfRH[abs(dfRH$RHz) < 3, ]
+dfHR <- dfHR[abs(dfHR$HRz) < 3, ]
+dfRR <- dfRR[abs(dfRR$RRz) < 3, ]
+
+dfZ <- cbind_fill(dfHH, dfRH, dfHR, dfRR,
+                  fill = NA)
+dfZ <- dfZ[, -c(2,4,6,8,9)]
+
+# grab mean and sd of dfZ
+mean <- sapply(dfZ, mean, na.rm = TRUE)
+sd <- sapply(dfZ, sd, na.rm = TRUE)
+pointsE_AMPrZ <- data.frame(Beta = names, mean = mean, sd = sd)
+
 # modular code
 mod <- bootE_DFGr
 
@@ -105,6 +130,31 @@ bplot_tight
 
 ggsave(here("output", "figures", "E_DFGr_bplot.png"), plot=bplot, device="png", dpi=300)
 ggsave(here("output", "figures", "E_DFGr_bplottight.png"), plot=bplot_tight, device="png", dpi=300)
+
+# dropping outliers and recalculating points
+df$HHz <- scale(df$`Hatchery(t)<<Hatchery(t-1)`)
+df$RHz <- scale(df$`River(t)<<Hatchery(t-1)`)
+df$HRz <- scale(df$`Hatchery(t)<<River(t-1)`)
+df$RRz <- scale(df$`River(t)<<River(t-1)`)
+
+dfHH <- df[, -c(2:4, 6:8)]
+dfRH <- df[, -c(1,3,4,5,7,8)]
+dfHR <- df[, -c(1,2,4,5,6,8)]
+dfRR <- df[, -c(1:3, 5:7)]
+
+dfHH <- dfHH[abs(dfHH$HHz) < 3, ]
+dfRH <- dfRH[abs(dfRH$RHz) < 3, ]
+dfHR <- dfHR[abs(dfHR$HRz) < 3, ]
+dfRR <- dfRR[abs(dfRR$RRz) < 3, ]
+
+dfZ <- cbind_fill(dfHH, dfRH, dfHR, dfRR,
+                  fill = NA)
+dfZ <- dfZ[, -c(2,4,6,8,9)]
+
+# grab mean and sd of dfZ
+mean <- sapply(dfZ, mean, na.rm = TRUE)
+sd <- sapply(dfZ, sd, na.rm = TRUE)
+pointsE_AMPrZ <- data.frame(Beta = names, mean = mean, sd = sd)
 
 # modular code
 mod <- bootE_DFGe
@@ -153,6 +203,31 @@ bplot_tight
 ggsave(here("output", "figures", "E_DFGe_bplot.png"), plot=bplot, device="png", dpi=300)
 ggsave(here("output", "figures", "E_DFGe_bplottight.png"), plot=bplot_tight, device="png", dpi=300)
 
+# dropping outliers and recalculating points
+df$HHz <- scale(df$`Hatchery(t)<<Hatchery(t-1)`)
+df$RHz <- scale(df$`River(t)<<Hatchery(t-1)`)
+df$HRz <- scale(df$`Hatchery(t)<<River(t-1)`)
+df$RRz <- scale(df$`River(t)<<River(t-1)`)
+
+dfHH <- df[, -c(2:4, 6:8)]
+dfRH <- df[, -c(1,3,4,5,7,8)]
+dfHR <- df[, -c(1,2,4,5,6,8)]
+dfRR <- df[, -c(1:3, 5:7)]
+
+dfHH <- dfHH[abs(dfHH$HHz) < 3, ]
+dfRH <- dfRH[abs(dfRH$RHz) < 3, ]
+dfHR <- dfHR[abs(dfHR$HRz) < 3, ]
+dfRR <- dfRR[abs(dfRR$RRz) < 3, ]
+
+dfZ <- cbind_fill(dfHH, dfRH, dfHR, dfRR,
+                  fill = NA)
+dfZ <- dfZ[, -c(2,4,6,8,9)]
+
+# grab mean and sd of dfZ
+mean <- sapply(dfZ, mean, na.rm = TRUE)
+sd <- sapply(dfZ, sd, na.rm = TRUE)
+pointsE_AMPrZ <- data.frame(Beta = names, mean = mean, sd = sd)
+
 # modular code
 mod <- bootO_AMPr
 
@@ -199,6 +274,31 @@ bplot_tight
 
 ggsave(here("output", "figures", "O_AMPr_bplot.png"), plot=bplot, device="png", dpi=300)
 ggsave(here("output", "figures", "O_AMPr_bplottight.png"), plot=bplot_tight, device="png", dpi=300)
+
+# dropping outliers and recalculating points
+df$HHz <- scale(df$`Hatchery(t)<<Hatchery(t-1)`)
+df$RHz <- scale(df$`River(t)<<Hatchery(t-1)`)
+df$HRz <- scale(df$`Hatchery(t)<<River(t-1)`)
+df$RRz <- scale(df$`River(t)<<River(t-1)`)
+
+dfHH <- df[, -c(2:4, 6:8)]
+dfRH <- df[, -c(1,3,4,5,7,8)]
+dfHR <- df[, -c(1,2,4,5,6,8)]
+dfRR <- df[, -c(1:3, 5:7)]
+
+dfHH <- dfHH[abs(dfHH$HHz) < 3, ]
+dfRH <- dfRH[abs(dfRH$RHz) < 3, ]
+dfHR <- dfHR[abs(dfHR$HRz) < 3, ]
+dfRR <- dfRR[abs(dfRR$RRz) < 3, ]
+
+dfZ <- cbind_fill(dfHH, dfRH, dfHR, dfRR,
+                  fill = NA)
+dfZ <- dfZ[, -c(2,4,6,8,9)]
+
+# grab mean and sd of dfZ
+mean <- sapply(dfZ, mean, na.rm = TRUE)
+sd <- sapply(dfZ, sd, na.rm = TRUE)
+pointsE_AMPrZ <- data.frame(Beta = names, mean = mean, sd = sd)
 
 # modular code
 mod <- bootO_DFGr
@@ -247,6 +347,31 @@ bplot_tight
 ggsave(here("output", "figures", "O_DFGr_bplot.png"), plot=bplot, device="png", dpi=300)
 ggsave(here("output", "figures", "O_DFGr_bplottight.png"), plot=bplot_tight, device="png", dpi=300)
 
+# dropping outliers and recalculating points
+df$HHz <- scale(df$`Hatchery(t)<<Hatchery(t-1)`)
+df$RHz <- scale(df$`River(t)<<Hatchery(t-1)`)
+df$HRz <- scale(df$`Hatchery(t)<<River(t-1)`)
+df$RRz <- scale(df$`River(t)<<River(t-1)`)
+
+dfHH <- df[, -c(2:4, 6:8)]
+dfRH <- df[, -c(1,3,4,5,7,8)]
+dfHR <- df[, -c(1,2,4,5,6,8)]
+dfRR <- df[, -c(1:3, 5:7)]
+
+dfHH <- dfHH[abs(dfHH$HHz) < 3, ]
+dfRH <- dfRH[abs(dfRH$RHz) < 3, ]
+dfHR <- dfHR[abs(dfHR$HRz) < 3, ]
+dfRR <- dfRR[abs(dfRR$RRz) < 3, ]
+
+dfZ <- cbind_fill(dfHH, dfRH, dfHR, dfRR,
+                  fill = NA)
+dfZ <- dfZ[, -c(2,4,6,8,9)]
+
+# grab mean and sd of dfZ
+mean <- sapply(dfZ, mean, na.rm = TRUE)
+sd <- sapply(dfZ, sd, na.rm = TRUE)
+pointsE_AMPrZ <- data.frame(Beta = names, mean = mean, sd = sd)
+
 # modular code
 mod <- bootO_DFGe
 
@@ -293,3 +418,28 @@ bplot_tight
 
 ggsave(here("output", "figures", "O_DFGe_bplot.png"), plot=bplot, device="png", dpi=300)
 ggsave(here("output", "figures", "O_DFGe_bplottight.png"), plot=bplot_tight, device="png", dpi=300)
+
+# dropping outliers and recalculating points
+df$HHz <- scale(df$`Hatchery(t)<<Hatchery(t-1)`)
+df$RHz <- scale(df$`River(t)<<Hatchery(t-1)`)
+df$HRz <- scale(df$`Hatchery(t)<<River(t-1)`)
+df$RRz <- scale(df$`River(t)<<River(t-1)`)
+
+dfHH <- df[, -c(2:4, 6:8)]
+dfRH <- df[, -c(1,3,4,5,7,8)]
+dfHR <- df[, -c(1,2,4,5,6,8)]
+dfRR <- df[, -c(1:3, 5:7)]
+
+dfHH <- dfHH[abs(dfHH$HHz) < 3, ]
+dfRH <- dfRH[abs(dfRH$RHz) < 3, ]
+dfHR <- dfHR[abs(dfHR$HRz) < 3, ]
+dfRR <- dfRR[abs(dfRR$RRz) < 3, ]
+
+dfZ <- cbind_fill(dfHH, dfRH, dfHR, dfRR,
+                  fill = NA)
+dfZ <- dfZ[, -c(2,4,6,8,9)]
+
+# grab mean and sd of dfZ
+mean <- sapply(dfZ, mean, na.rm = TRUE)
+sd <- sapply(dfZ, sd, na.rm = TRUE)
+pointsE_AMPrZ <- data.frame(Beta = names, mean = mean, sd = sd)
