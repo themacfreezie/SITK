403e5664bdd18b31013d7f5cc8d5d930690de6ea
Brian E McGreal <bmcgreal@uw.edu>
2026-02-12 09:27:23 -0800

    building full model

--- code/primary/BETA_modelexplore.R
+++ code/primary/BETA_modelexplore.R
@@ -109,18 +109,97 @@ inits.O <- list(x0 = matrix(c(11.7200578, 9.4813332), nrow = 2))
 # ##  average of first 5 obs? - ask mark
 
 # run modelos
-ssEbeta_DFGob <- MARSS(datE_DFGob, 
-                         model = model.listE, 
-                         method = "kem")
-saveRDS(ssEbeta_DFGob, file=here("data", "clean", "ssEbeta_DFGob.rds"))
+# ssEbeta_DFGob <- MARSS(datE_DFGob, 
+#                          model = model.listE, 
+#                          method = "kem")
+# saveRDS(ssEbeta_DFGob, file=here("data", "clean", "ssEbeta_DFGob.rds"))
+# 
+# 
+# ssObeta_DFGob <- MARSS(datO_DFGob, 
+#                          model = model.listO,
+#                          # inits = inits.O,
+#                          control = list(
+#                                         # safe=TRUE,
+#                                         maxit = 1000
+#                                         ),
+#                          method = "kem")
+# saveRDS(ssObeta_DFGob, file=here("data", "clean", "ssObeta_DFGob.rds"))
 
+# combining datasets into one model
+# dat
+new_names <- c("site_run", 
+               "ct_1", "ct_2", "ct_3", "ct_4", "ct_5", "ct_6",
+               "ct_7", "ct_8", "ct_9", "ct_10", "ct_11", "ct_12",
+               "ct_13", "ct_14", "ct_15", "ct_16", "ct_17", "ct_18",
+               "ct_19", "ct_20", "ct_21", "ct_22", "ct_23", "ct_24")
+names(WbetaE_DFGob.df) <- new_names
+names(WbetaO_DFGob.df) <- new_names
+WbetaE_DFGob.df$site_run <- c("SJ_E", "IR_E")
+WbetaO_DFGob.df$site_run <- c("SJ_O", "IR_O")
+beta_DFGob.df <- bind_rows(WbetaE_DFGob.df, WbetaO_DFGob.df)
+dat_DFGob <- data.matrix(beta_DFGob.df[2:ncol(beta_DFGob.df)])
 
-ssObeta_DFGob <- MARSS(datO_DFGob, 
-                         model = model.listO,
-                         # inits = inits.O,
-                         control = list(
-                                        # safe=TRUE,
-                                        maxit = 1000
-                                        ),
-                         method = "kem")
-saveRDS(ssObeta_DFGob, file=here("data", "clean", "ssObeta_DFGob.rds"))
+# smolts
+new_names <- c("ct_1", "ct_2", "ct_3", "ct_4", "ct_5", "ct_6",
+               "ct_7", "ct_8", "ct_9", "ct_10", "ct_11", "ct_12",
+               "ct_13", "ct_14", "ct_15", "ct_16", "ct_17", "ct_18",
+               "ct_19", "ct_20", "ct_21", "ct_22", "ct_23", "ct_24")
+names(WsmoltsE) <- new_names
+names(WsmoltsO) <- new_names
+smolts <- bind_rows(WsmoltsE, WsmoltsO)
+smolts <- data.matrix(smolts)
+
+# model list
+z.model <- "identity"
+u.model <- "zero"
+a.model <- "zero"
+x.model <- "unequal"
+v.model <- "zero"
+
+# site level observation error
+r.model <- matrix(list(0), 4, 4)
+r.model[1,1] <- "rH"
+r.model[2,2] <- "rR"
+r.model[3,3] <- "rH"
+r.model[4,4] <- "rR"
+
+# autoregressive character - beta matrix
+b.model <- matrix(list(0), 4, 4)
+b.model[1,2] <- "bE_Ht<Rt-1"
+b.model[2,1] <- "bE_Rt<Ht-1"
+b.model[2,2] <- "bE_RR"
+b.model[3,4] <- "bO_Ht<Rt-1"
+b.model[4,3] <- "bO_Rt<Ht-1"
+b.model[4,4] <- "bO_RR"
+
+# c matrix - for them smolts
+c.model <- matrix(list(0), 4, 2)
+c.model[1,1] <- "sE"
+c.model[3,2] <- "sO"
+
+# run level observation error
+q.model <- matrix(list(0), 4, 4)
+r.model[1,1] <- "qE"
+r.model[2,2] <- "qE"
+r.model[3,3] <- "qO"
+r.model[4,4] <- "qO"
+
+model.list <- list(
+  B = b.model 
+  , U = u.model
+  , Q = q.model
+  , Z = z.model 
+  , A = a.model 
+  , R = r.model
+  , x0 = x.model 
+  , V0 = v.model 
+  , tinitx = 0
+  , C = c.model
+  , c = smoltsO
+)
+
+# run modelos
+ssbeta_DFGob <- MARSS(dat_DFGob,
+                      model = model.list,
+                      method = "kem")
+saveRDS(ssbeta_DFGob, file=here("data", "clean", "ssbeta_DFGob.rds"))
