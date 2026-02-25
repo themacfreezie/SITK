7b037aaabaaba398a074d06f3c7baed1ede9da0d
Brian E McGreal <bmcgreal@uw.edu>
2026-02-24 19:27:36 -0800

    h

--- code/primary/20-NSEout_comparison.R
+++ code/primary/20-NSEout_comparison.R
@@ -135,17 +135,11 @@ forecast::ndiffs(IR_e, test= "kpss")
 #
 # let's try and apply this...
 
-k <- trunc((length(diffNS_e) - 1)^(1/3))
-testNS_e <- urca::ur.df(diffNS_e, type = "drift", lags = k)
-
-k <- trunc((length(diffIR_e) - 1)^(1/3))
-testIR_e <- urca::ur.df(diffIR_e, type = "drift", lags = k)
-
-k <- trunc((length(diffNS_e) - 1)^(1/3))
-testNS_o <- urca::ur.df(diffNS_o, type = "drift", lags = k)
-
-k <- trunc((length(diffIR_e) - 1)^(1/3))
-testIR_o <- urca::ur.df(diffIR_o, type = "drift", lags = k)
+k1 <- trunc((length(diffNS_e) - 1)^(1/3)) # all same length
+testNS_e <- urca::ur.df(diffNS_e, type = "drift", lags = k1)
+testIR_e <- urca::ur.df(diffIR_e, type = "drift", lags = k1)
+testNS_o <- urca::ur.df(diffNS_o, type = "drift", lags = k1)
+testIR_o <- urca::ur.df(diffIR_o, type = "drift", lags = k1)
 
 summary(testNS_e)
   # stationary
@@ -156,179 +150,163 @@ summary(testNS_o)
 summary(testIR_o)
   # non-stationary (close)
 
-
-
-
-# Box-Jenkins method suggests taking a second difference...
-dif2NS_e <- diff(diffNS_e)
-dif2IR_e <- diff(diffIR_e)
+# test diff2 for odd years?
 dif2NS_o <- diff(diffNS_o)
 dif2IR_o <- diff(diffIR_o)
 
-# stationarity tests for double-differenced data - confirm
-adf.test(dif2NS_e)
+k2 <- trunc((length(dif2NS_o) - 1)^(1/3)) # all same length
+tst2NS_o <- urca::ur.df(dif2NS_o, type = "drift", lags = k2)
+tst2IR_o <- urca::ur.df(dif2IR_o, type = "drift", lags = k2)
+
+summary(tst2NS_o)
+  # non-stationary (closer)
+summary(tst2IR_o)
+  # non-stationary (closer)
+
+# test diff3 for odd years?
+dif3NS_o <- diff(dif2NS_o)
+dif3IR_o <- diff(dif2IR_o)
+
+k3 <- trunc((length(dif3NS_o) - 1)^(1/3)) # all same length
+tst3NS_o <- urca::ur.df(dif3NS_o, type = "drift", lags = k3)
+tst3IR_o <- urca::ur.df(dif3IR_o, type = "drift", lags = k3)
+
+summary(tst3NS_o)
   # stationary
-adf.test(dif2IR_e)
-  # stationary
-adf.test(dif2NS_o)
-  # non-stationary
-adf.test(dif2IR_o)
-  # non-stationary
-
-kpss.test(dif2NS_e)
-  # stationary
-kpss.test(dif2IR_e)
-  # stationary
-kpss.test(dif2NS_o)
-  # stationary
-kpss.test(dif2IR_o)
-  # stationary
-
-
-
-# differenced time series are 'trend stationary' (i.e. passing kpss but not adf)
-# except differenced NS_e which is stationary
-
-# what about detrending data?
-time_index <- seq_along(diffNS_e)
-lmNS_e <- lm(diffNS_e ~ time_index)
-lmIR_e <- lm(diffIR_e ~ time_index)
-lmNS_o <- lm(diffNS_o ~ time_index)
-lmIR_o <- lm(diffIR_o ~ time_index)
-
-detrendedNS_e <- residuals(lmNS_e)
-detrendedIR_e <- residuals(lmIR_e)
-detrendedNS_o <- residuals(lmNS_o)
-detrendedIR_o <- residuals(lmIR_o)
-
-# stationarity tests for differenced data - confirm
-adf.test(detrendedNS_e)
-   # stationary
-adf.test(detrendedIR_e)
-   # non-stationary
-adf.test(detrendedNS_o)
-  # non-stationary
-adf.test(detrendedIR_o)
-  # non-stationary
-
-kpss.test(detrendedNS_e)
-   # stationary
-kpss.test(detrendedIR_e)
-   # stationary
-kpss.test(detrendedNS_o)
-  # stationary
-kpss.test(detrendedIR_o)
+summary(tst3IR_o)
   # stationary
 
-# why would detrended data have the exact same result in an adf or kpss test as before detrending?
-# possible because of high-order autocorrelation
+## MUST TALK TO MARK ABOUT THIS...
 
-# autocorrelation
-acf(NS_e)
-acf(IR_e)
-acf(NS_o)
-acf(IR_o)
-  # high-order for sure, all time series have at least 4 significant lags
-  # this makes sense given that these are generations of fish
+# so, diffNS_e & diffIR_e are stationary
+# and dif3NS_o & dif3IR_o are stationary
+# AND diffNS_o & diffIR_o are close to stationary
 
+# EVEN RUNS
+# let's check mean and variance
+mean(diffNS_e) # 0.06072951
+mean(diffIR_e) # 0.0725145
+var(diffNS_e) # 0.1257533
+var(diffIR_e) # 0.0873107
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# if stationary, let's check mean and variance
-mean(NS_e) # -0.2301661
-mean(IR_e) # -0.4252932
-var(NS_e) # 0.8847487
-var(IR_e) # 1.057261
-
-mean(NS_o) # 0.2877304
-mean(IR_o) # -0.3054424
-var(NS_o) # 0.456703
-var(IR_o) # 1.115295
-
-# autocorrelation
-acfNS_e <- acf(NS_e, plot = TRUE) # 5 sig lags
-acfIR_e <- acf(IR_e, plot = TRUE) # 7 sig lags
-acfNS_o <- acf(NS_o, plot = TRUE) # 3 sig lags
-acfIR_o <- acf(IR_o, plot = TRUE) # 4 sig lags
-
-# t.tests - means
-t.test(NS_e, IR_e, var.equal = FALSE) # NO SIGNIFICANT DIFFERENCE
-t.test(NS_o, IR_o, var.equal = FALSE) # SIGNIFICANT DIFFERENCE
+# check autocorrelation
+acfNS_e <- acf(diffNS_e, plot = TRUE) # 0 sig lags
+acfIR_e <- acf(diffIR_e, plot = TRUE) # 1 sig lags (@ lag4, seems random)
 
 # F-test - variance
-var.test(NS_e, IR_e) # NO SIGNIFICANT DIFFERENCE
-var.test(NS_o, IR_o) # SIGNIFICANT DIFFERENCE
+var.test(diffNS_e, diffIR_e) # NO SIGNIFICANT DIFFERENCE IN VARIANCE
 
-# # partial autocorrelation
-# pacf(NS_e)
-# pacf(IR_e)
-# pacf(NS_o)
-# pacf(IR_o)
-# 
-# # differences?
-# diffNS_e <- diff(NS_e,
-#                  lag = 1,
-#                  differences = 1)
-# diffIR_e <- diff(IR_e,
-#                  lag = 1,
-#                  differences = 1)
-# diffNS_o <- diff(NS_o,
-#                  lag = 1,
-#                  differences = 1)
-# diffIR_o <- diff(IR_o,
-#                  lag = 1,
-#                  differences = 1)
-# plot(diffNS_e)
-# plot(diffIR_e)
-# plot(diffNS_o)
-# plot(diffIR_o)
-# 
-# # autocorrelation
-# acf(diffNS_e)
-# acf(diffIR_e)
-# acf(diffNS_o)
-# acf(diffIR_o)
-# 
-# # partial autocorrelation
-# pacf(diffNS_e)
-# pacf(diffIR_e)
-# pacf(diffNS_o)
-# pacf(diffIR_o)
-# 
-# ## cross-correlation function
-# ccf(NS_e, IR_e)
-# ccf(NS_o, IR_o)
-# 
-# ## arima models
-# ARIMA.NS_e <- auto.arima(NS_e)
-# summary(ARIMA.NS_e)
-# ARIMA.IR_e <- auto.arima(IR_e)
-# summary(ARIMA.IR_e)
-# 
-# ARIMA.NS_o <- auto.arima(NS_o)
-# summary(ARIMA.NS_o)
-# ARIMA.IR_o <- auto.arima(IR_o)
-# summary(ARIMA.IR_o)
-#   # not sure what this tells me
-# 
-# ## dynamic time warping
-# # Compute the DTW alignment
-# alignmentE <- dtw(NS_e, IR_e, dist.method = "Euclidean")
-# alignmentO <- dtw(NS_o, IR_o, dist.method = "Euclidean")
-# 
-# # Plot the results
-# plot(alignmentE)
-# abline(a = 0, b = 1, lty = "dotted", col = "red")
-# plot(alignmentO)
-# abline(a = 0, b = 1, lty = "dotted", col = "red")
+# t.test - means
+t.test(diffNS_e, diffIR_e, var.equal = TRUE) # NO SIGNIFICANT DIFFERENCE in MEANS
+  # var.equal = TRUE specified due to F-test finding no difference
+  # var.equal = FALSE also finds no significant difference
+
+# trend (that was removed?)
+t <- 1:length(NS_e)
+modNS_e <- lm(NS_e ~ t)
+summary(modNS_e)
+
+modIR_e <- lm(IR_e ~ t)
+summary(modIR_e)
+
+# do these trends differ? Welch's t-test
+b_NS <- summary(modNS_e)$coefficients["t", "Estimate"]
+se_NS <- summary(modNS_e)$coefficients["t", "Std. Error"]
+
+b_IR <- summary(modIR_e)$coefficients["t", "Estimate"]
+se_IR <- summary(modIR_e)$coefficients["t", "Std. Error"]
+
+b_diff <- b_IR - b_NS
+se_diff <- sqrt(se_IR^2 + se_NS^2)
+
+# calculate t-stat and p value
+t_stat <- b_diff / se_diff
+p_value <- 2 * (1 - pnorm(abs(t_stat)))
+p_value  # 0.3593233 - # NO SIGNIFICANT DIFFERENCE IN TRENDS
+
+
+
+# ODD RUNS - 3rd order differencing?
+# let's check mean and variance
+mean(dif3NS_o) # 0.02877715
+mean(dif3IR_o) # -0.001083714
+var(dif3NS_o) # 1.144709
+var(dif3IR_o) # 0.9271075
+
+# check autocorrelation
+acfNS_o <- acf(dif3NS_o, plot = TRUE) # 1st lag is signif
+acfIR_o <- acf(dif3IR_o, plot = TRUE) # 1st lag is signif, 2 more significant
+
+# F-test - variance
+var.test(dif3NS_o, dif3IR_o) # NO SIGNIFICANT DIFFERENCE IN VARIANCE
+
+# t.test - means
+t.test(dif3NS_o, dif3IR_o, var.equal = TRUE) # NO SIGNIFICANT DIFFERENCE IN MEANS
+  # var.equal = TRUE specified due to F-test finding no difference
+  # var.equal = FALSE also finds no significant difference
+
+# trend (that was removed?)
+t <- 1:length(NS_o)
+modNS_o <- lm(NS_o ~ poly(t,3))
+summary(modNS_o)
+
+modIR_o <- lm(IR_o ~ poly(t,3))
+summary(modIR_o)
+
+# do these trends differ? Welch's t-test
+b_NS <- summary(modNS_o)$coefficients["poly(t, 3)1", "Estimate"]
+se_NS <- summary(modNS_o)$coefficients["poly(t, 3)1", "Std. Error"]
+
+b_IR <- summary(modIR_o)$coefficients["poly(t, 3)1", "Estimate"]
+se_IR <- summary(modIR_o)$coefficients["poly(t, 3)1", "Std. Error"]
+
+b_diff <- b_IR - b_NS
+se_diff <- sqrt(se_IR^2 + se_NS^2)
+
+# calculate t-stat and p value
+t_stat <- b_diff / se_diff
+p_value <- 2 * (1 - pnorm(abs(t_stat)))
+p_value  # 0.005148745 - # SIGNIFICANT DIFFERENCE IN TRENDS
+
+
+# ODD RUN - 1st order differencing (close to being stationary)
+# let's check mean and variance
+mean(diffNS_o) # 0.05492173
+mean(diffIR_o) # 0.07704178
+var(diffNS_o) # 0.1615338
+var(diffIR_o) # 0.200944
+
+# check autocorrelation
+acfNS_o <- acf(diffNS_o, plot = TRUE) # 1 sig lags (@ lag5, seems random)
+acfIR_o <- acf(diffIR_o, plot = TRUE) # 1 sig lags (@ lag5, seems random)
+
+# F-test - variance
+var.test(diffNS_o, diffIR_o) # NO SIGNIFICANT DIFFERENCE IN VARIANCE
+
+# t.test - means
+t.test(diffNS_o, diffIR_o, var.equal = TRUE) # NO SIGNIFICANT DIFFERENCE IN MEANS
+  # var.equal = TRUE specified due to F-test finding no difference
+  # var.equal = FALSE also finds no significant difference
+
+# trend (that was removed?)
+t <- 1:length(NS_o)
+modNS_o <- lm(NS_o ~ t)
+summary(modNS_o)
+
+modIR_o <- lm(IR_o ~ t)
+summary(modIR_o)
+
+# do these trends differ? Welch's t-test
+b_NS <- summary(modNS_o)$coefficients["t", "Estimate"]
+se_NS <- summary(modNS_o)$coefficients["t", "Std. Error"]
+
+b_IR <- summary(modIR_o)$coefficients["t", "Estimate"]
+se_IR <- summary(modIR_o)$coefficients["t", "Std. Error"]
+
+b_diff <- b_IR - b_NS
+se_diff <- sqrt(se_IR^2 + se_NS^2)
+
+# calculate t-stat and p value
+t_stat <- b_diff / se_diff
+p_value <- 2 * (1 - pnorm(abs(t_stat)))
+p_value  # 0.007193675 - # SIGNIFICANT DIFFERENCE IN TRENDS
